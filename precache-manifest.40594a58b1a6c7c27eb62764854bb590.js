162580142e01c4d8c746936e37f603b7aeb9471e
turbopope <PapstDonB@Googlemail.com>
2019-08-23 18:10:09 +0800

    Updates

---
+++ precache-manifest.40594a58b1a6c7c27eb62764854bb590.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0de52fcd443377c4f61a97978951938c",
+    "url": "/reactive-smiles-drawer/index.html"
+  },
+  {
+    "revision": "e51a1990ee7b9ac91e1a",
+    "url": "/reactive-smiles-drawer/static/css/main.d16b9f11.chunk.css"
+  },
+  {
+    "revision": "bf3e71adc744eff6a29c",
+    "url": "/reactive-smiles-drawer/static/js/2.3ee6d4db.chunk.js"
+  },
+  {
+    "revision": "e51a1990ee7b9ac91e1a",
+    "url": "/reactive-smiles-drawer/static/js/main.205a7145.chunk.js"
+  },
+  {
+    "revision": "c469842fcce118e3ee93",
+    "url": "/reactive-smiles-drawer/static/js/runtime~main.1d046d94.js"
+  }
+]);
